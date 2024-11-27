baab073b21cc5d4b0f3de2c2f31dc0825191b1a9
Anubhav Bhatt <anubhavbhatt@MacBookAir.lan>
2024-11-27 20:15:45 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+
+import {PrismaClient} from @repo/db/clientInformation;
+
+
+
+
+export default function Page(): JSX.Element {
+  return (
+    <div className="text-2xl">
+      hi there
+    </div>
+  );
+}
